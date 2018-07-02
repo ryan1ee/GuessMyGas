b1cc6f521d29bf2053e214b1e7c349eda763b67e
ryanptss <ryanptss@gmail.com>
2018-07-02 02:56:18 -0700

    Added Snackbar

--- app/src/main/java/com/marcoryan/guessmygas/MainActivity.java
+++ app/src/main/java/com/marcoryan/guessmygas/MainActivity.java
@@ -3,9 +3,13 @@ package com.marcoryan.guessmygas;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.util.Log;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.BaseTransientBottomBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    Snackbar.make(findViewById(android.R.id.content), "Welcome to Dashboard!", Snackbar.LENGTH_LONG).show();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    Snackbar.make(findViewById(android.R.id.content), "Welcome to Notifications!", Snackbar.LENGTH_LONG).show();
                     return true;
             }
             return false;
@@ -39,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        System.out.println("test!");
 
-    // hello there
+    }
 
 }
