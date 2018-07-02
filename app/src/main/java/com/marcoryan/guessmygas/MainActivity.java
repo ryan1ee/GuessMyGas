1c3ced4a75eabce75ba9edd05f5c8200b4d2f153
ryanptss <ryanptss@gmail.com>
2018-07-02 02:27:59 -0700

    test commit 2

--- app/src/main/java/com/marcoryan/guessmygas/MainActivity.java
+++ app/src/main/java/com/marcoryan/guessmygas/MainActivity.java
@@ -41,6 +41,6 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    // marco is gay
+    // hello there
 
 }
